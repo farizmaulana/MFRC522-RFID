75139f00ee43e0a39b980724d94f9812275b7c54
Fariz Maulana <farizmaulana1@gmail.com>
2016-01-20 08:17:46 +0700

    Add ReadCard.ino to read the data stored in the RFID Tag

---
+++ ReadCard/ReadCard.ino
@@ -0,0 +1,39 @@
+/*  
+ * Write personal data of a MIFARE RFID card using a RFID-RC522 reader
+ * Uses MFRC522 - Library to use ARDUINO RFID MODULE KIT 13.56 MHZ WITH TAGS SPI W AND R BY COOQROBOT. 
+ * -----------------------------------------------------------------------------------------
+ *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
+ *             Reader/PCD   Uno           Mega      Nano v3    Leonardo/Micro   Pro Micro
+ * Signal      Pin          Pin           Pin       Pin        Pin              Pin
+ * -----------------------------------------------------------------------------------------
+ * RST/Reset   RST          8             5         D9         RESET/ICSP-5     RST
+ * SPI SS      SDA(SS)      10            53        D10        10               10
+ * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
+ * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
+ * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
+ *
+ * Hardware required:
+ * Arduino
+ * PCD (Proximity Coupling Device): NXP MFRC522 Contactless Reader IC
+ * PICC (Proximity Integrated Circuit Card): A card or tag using the ISO 14443A interface, eg Mifare or NTAG203.
+ * The reader can be found on eBay for around 5 dollars. Search for "mf-rc522" on ebay.com. 
+ */
+
+ #include <SPI.h>
+ #include <MFRC522.h>
+
+ #define SS_PIN   10            // Configurable, see typical pin layout above
+ #define RST_PIN  8             // Configurable, see typical pin layout above
+ MFRC522 rfid(SS_PIN, RST_PIN); // Create MFRC522 Instance
+
+ void setup() {
+    Serial.begin(9600);
+    SPI.begin();
+    rfid.PCD_Init();
+    Serial.println("Menulis data pada RFID Card");
+}
+
+void loop() {
+  
+}
+
